bf56c955e05b3357f4c62a8b84de67f48f57c8fc
Beniamin Jedrychowski <bj394329@students.mimuw.edu.pl>
2020-11-08 23:24:00 +0100

    edit on scripts that execute python

--- src/insc_jvm.sh
+++ src/insc_jvm.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-source env/bin/activate
-python3 ./src/Interpreter.py "$*"
-outfile="${*%.ins}.j"
-if test -f "$outfile"; then
-  java -jar ./lib/jasmin.jar -d "$*/.." "$outfile"
-fi
-echo "$outfile"
+	source env/bin/activate; \
+	python3 ./src/Interpreter.py "$@"; \
+	outfile="${@%.ins}.j"; \
+	if test -f "$outfile"; then
+	  java -jar ./lib/jasmin.jar -d "$*/.." "$outfile"
+	fi
